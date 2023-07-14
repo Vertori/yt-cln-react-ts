2266562442ca52822b76b94d262cf418ef8c57e8
Vertori <115083330+Vertori@users.noreply.github.com>
2023-07-14 14:30:20 +0200

    website scheme /w routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import './App.css'
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 
-function App() {
-
+function App(): JSX.Element {
   return (
-    <>
-
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
