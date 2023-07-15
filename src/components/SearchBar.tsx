27d125716cd975d61e0b231c7b03ff0f78ec57ce
Vertori <115083330+Vertori@users.noreply.github.com>
2023-07-15 10:58:48 +0200

    searchbar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
 
-const Searchbar = () => {
+const Searchbar = (): JSX.Element => {
   return (
-    <div>Searchbar</div>
-  )
-}
+    <form className="w-full max-w-md flex flex-row items-center ml-8">
+      <input
+        className="w-full h-11 bg-[#f5f5f5] outline-none border-none rounded-3xl pt-0 pr-14 pb-0 pl-6 text-base"
+        type="text"
+        placeholder="Search..."
+        onChange={() => {}}
+      />
+      <button className="w-14 h-11 -ml-10 bg-none border-none outline-none cursor-pointer transition hover:text-red-600">
+        <BsSearch />
+      </button>
+    </form>
+  );
+};
 
-export default Searchbar
+export default Searchbar;
