27eda30fa978a668f3cec53706b6bd72b29300c8
Vertori <115083330+Vertori@users.noreply.github.com>
2023-07-15 11:14:38 +0200

    sidebar component

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -6,7 +6,9 @@ const Feed = (): JSX.Element => {
     <div className="flex flex-col md:flex-row ">
       <div className="h-auto md:h-[92vh] border-r border-solid border-[#3d3d3d] px-0 md:px-8">
         <Sidebar />
-        <div className="text-white">Copyright © RF & Youtube</div>
+        <div className="text-white hidden md:block">
+          Copyright © RF & Youtube
+        </div>
       </div>
       <div className="p-8 overflow-y-auto h-[90vh] flex-2">
         <div className="text-white text-3xl mb-8 font-bold">
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { categories } from "../utils/constants";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+const selectedCategory = "New";
+
+const Sidebar = (): JSX.Element => {
+  return (
+    <div className="flex flex-row md:flex-col overflow-y-auto h-auto md:h-[95%] gap-4 md:gap-6 scrollbar-hide mx-4 md:mx-0 md:py-6">
+      {categories.map((category) => (
+        <button
+          className={`${
+            category.name === selectedCategory && "bg-[#FC1503]"
+          } flex justify-start items-center text-white rounded-full px-3 py-3 hover:bg-[#FC1503] group md:mx-4`}
+          key={category.name}
+        >
+          <span
+            className={`${
+              category.name === selectedCategory ? "text-white" : "text-red-700"
+            } mr-[10px] group-hover:text-white`}
+          >
+            {<category.icon size={20} />}
+          </span>
+          <span>{category.name}</span>
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default Sidebar;
