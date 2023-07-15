372031faad50e995156474608d2748883ddabe50
Vertori <115083330+Vertori@users.noreply.github.com>
2023-07-15 10:55:15 +0200

    navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import ytLogo from "../assets/yt-icon.png";
+import Searchbar from "./SearchBar";
 
-const Navbar = () => {
+const Navbar = (): JSX.Element => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="w-full h-[90px] bg-[#000] flex justify-between items-center sticky top-0 py-4 px-6">
+      <Link to="/">
+        <img className="w-[60px]" src={ytLogo} alt="YouTube Logo" />
+      </Link>
+      <Searchbar />
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ src/utils/constants.ts
@@ -0,0 +1,38 @@
+import { AiFillHome } from "react-icons/ai";
+import { FiCode } from "react-icons/fi";
+import { BsMusicNoteBeamed } from "react-icons/bs";
+import { IoMdSchool } from "react-icons/io";
+import { BiPodcast } from "react-icons/bi";
+import { BiCameraMovie } from "react-icons/bi";
+import { FaGamepad } from "react-icons/fa";
+import { MdLiveTv } from "react-icons/md";
+import { FaFootballBall } from "react-icons/fa";
+import { TbHanger } from "react-icons/tb";
+import { MdFace3 } from "react-icons/md";
+import { MdTheaterComedy } from "react-icons/md";
+import { TbBarbell } from "react-icons/tb";
+import { FaBitcoin } from "react-icons/fa";
+
+export const categories = [
+  { name: "New", icon: AiFillHome },
+  { name: "Coding", icon: FiCode },
+  { name: "Music", icon: BsMusicNoteBeamed },
+  { name: "Education", icon: IoMdSchool },
+  { name: "Podcast", icon: BiPodcast },
+  { name: "Movie", icon: BiCameraMovie },
+  { name: "Gaming", icon: FaGamepad },
+  { name: "Live", icon: MdLiveTv },
+  { name: "Sport", icon: FaFootballBall },
+  { name: "Fashion", icon: TbHanger },
+  { name: "Beauty", icon: MdFace3 },
+  { name: "Comedy", icon: MdTheaterComedy },
+  { name: "Gym", icon: TbBarbell },
+  { name: "Crypto", icon: FaBitcoin },
+];
+
+export const demoThumbnailUrl = 'https://d-art.ppstatic.pl/kadry/k/r/1/29/87/62a0a7e0a9b93_o_medium.jpg';
+export const demoChannelUrl = '/channel/UC0gOiF-21O9Mct1AFpPeXfQ';
+export const demoVideoUrl = '/video/XdKzUbAiswE';
+export const demoChannelTitle = 'Disney Studios';
+export const demoVideoTitle = 'Turning Red | Official Trailer';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
